a414a65d571ea06167bff1c0b3b722f80795d040
Patryk Bieszke <peinpat1@gmail.com>
2020-05-23 20:29:23 +0200

    PQS-3: Port fix

--- src/start.ts
+++ src/start.ts
@@ -3,5 +3,5 @@ import { DawApiServer } from './DawApi/DawApiServer';
 // Start the server or run tests
 if (process.argv[2] !== 'test') {
     const server = new DawApiServer();
-    server.start(3000);
+    server.start(parseInt(process.env.PORT as string) || 3000);
 }
