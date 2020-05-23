b45bf7cba808bc77c913705b224e7f4300e02e67
Patryk Bieszke <peinpat1@gmail.com>
2020-05-23 21:35:57 +0200

    PQS-3: Test of build

--- src/webpackCfg/webpack.config.ts
+++ src/webpackCfg/webpack.config.ts
@@ -4,11 +4,9 @@ import * as webpack from 'webpack';
 // List of allowed environments
 type NODE_ENV = 'local' | 'production';
 
-const webpackConfig: webpack.Configuration =
-    {
-        local: webpackLocalSettings,
-        production: webpackProductionSettings,
-    }[process.env.NODE_ENV as NODE_ENV] || {};
-
+const webpackConfig: webpack.Configuration = {
+    local: webpackLocalSettings,
+    production: webpackProductionSettings,
+}[(process.env.NODE_ENV as NODE_ENV) || 'production'];
 module.exports = webpackConfig;
 module.exports.mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
